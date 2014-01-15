102860c3f812057a5a68447c9d3bbe0bafef77fc
Dustin Quam <quamsta@gmail.com>
2014-01-15 17:11:43 -0600

    tons of things

---
+++ Gruntfile.js
@@ -0,0 +1,80 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      dist: {
+        src: [
+          'division-simple/bower_components/jquery/jquery.js',
+          'division-simple/bower_components/magnific-popup/dist/jquery.magnific-popup.js',
+          'division-simple/vendor/sass-bootstrap/sass-bootstrap.min.js',
+          'division-bar/js/division-bar.js',
+          'division-simple/js/*.js',
+          'themes/captureiowa/js/*.js'
+        ],
+        dest: 'themes/captureiowa/js/build/main.js'
+      }
+    },
+
+    uglify: {
+      build: {
+        src: ['themes/captureiowa/js/build/main.js'],
+        dest: 'themes/captureiowa/js/build/main.min.js'
+      }
+    },
+
+    compass: {
+      dist: {                   // Target
+        options: {              // Target options
+          sassDir: 'themes/captureiowa/scss',
+          cssDir: 'themes/captureiowa/css',
+          environment: 'production'
+        }
+      }
+    },
+
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'themes/captureiowa/images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'themes/captureiowa/images/'
+        }]
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['division-simple/js/*.js', 'division-simple/js/**/*.js'],
+        tasks: ['concat', 'uglify'],
+        options: {
+          spawn: false,
+        }
+      },
+      css: {
+        files: ['themes/captureiowa/scss/*.scss', 'themes/captureiowa/scss/**/*.scss'],
+        tasks: ['compass'],
+        options: {
+          spawn: false,
+        }
+      }
+    },
+
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-simple-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['concat', 'uglify', 'compass', 'simple-watch']);
+
+};
---
+++ themes/captureiowa/templates/Includes/HeroArea.ss
@@ -0,0 +1,16 @@
+<div class="hero-instructions">
+	<div class="container">
+		<p>Capture Iowa is a a photo project that captures the Class of 2017's first year at UI and encourages students to share the ways they engage on campus!</p>
+
+		<h2>How to Submit Your Photos</h2>
+
+		<p>Simply use the hashtag '#captureiowa' on any social media and check back here for your photo.</p>
+		<p>At the end of the year, select photos will be chosen and shown in a final gallery display in Calvin Hall</p>
+	</div>
+</div>
+
+<div class="container">
+	<div class="storify">
+		<iframe src="//storify.com/CaptureIowa/capture-iowa/embed?header=false" width="100%" height=750 frameborder=no allowtransparency=true></iframe><script src="//storify.com/CaptureIowa/capture-iowa.js?header=false"></script><noscript>[<a href="//storify.com/CaptureIowa/capture-iowa" target="_blank">View the story "Capture Iowa" on Storify</a>]</noscript>
+	</div>
+</div>
