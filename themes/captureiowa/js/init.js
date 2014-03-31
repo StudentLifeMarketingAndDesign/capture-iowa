19ab61fa45651a1569136a3fef07611ec5ff23cc
Cole Mecum <mecum.cole@gmail.com>
2014-03-31 11:21:32 -0500

    committing cole's stuff - dustin

--- themes/captureiowa/js/init.js
+++ themes/captureiowa/js/init.js
@@ -1,14 +1,24 @@
-$(window).scroll(function() {
-    if(($(window).scrollTop() > 10) && ($( window ).width() > 768)){
-         $('.division-directory').slideUp();
+
+jQuery(document).ready(function($){
+    var deviceAgent = navigator.userAgent.toLowerCase();
+    var agentID = deviceAgent.match(/(iPad|iPhone|iPod)/i);
+    var target =  $(window);
+    if (agentID)
+    {
+        target = $(body);
     }
 
-    if(($(window).scrollTop() > 160) && ($( window ).width() > 768)){
-         $('.logo').addClass('scrolled');
-    }
+    $(window).scroll(function() {
+        if(($(window).scrollTop() > 10) && ($( window ).width() > 768)){
+            $('.division-directory').slideUp();
+        }
 
-    if(($(window).scrollTop() < 160) && ($( window ).width() > 768)){
-         $('.logo').removeClass('scrolled');
-    }    
+        if(($(window).scrollTop() > 160) && ($( window ).width() > 768)){
+            $('.logo').addClass('scrolled');
+        }
+        if(($(window).scrollTop() < 160) && ($( window ).width() > 768)){
+            $('.logo').removeClass('scrolled');
+        }
 
+    });
 });
