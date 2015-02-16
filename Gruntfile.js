c820232395e58a57d0127368859834cb5dcfcb14
Dustin Quam <dustinquam@me.com>
2015-02-16 12:15:43 -0600

    updated modules, switched slideshow format

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,18 @@
 module.exports = function(grunt) {
+  var globalConfig = {
+    themeDir: 'themes/captureiowa'
+  };
 
   // Project configuration.
   grunt.initConfig({
-
+    globalConfig: globalConfig,
     pkg: grunt.file.readJSON('package.json'),
 
     concat: {
       dist: {
         src: [
-          'division-simple/bower_components/magnific-popup/dist/jquery.magnific-popup.js',
-          'division-simple/vendor/sass-bootstrap/js/sass-bootstrap.min.js',
+          'bower_components/bootstrap-sass/javascript/bootstrap.js',
           'division-bar/js/division-bar.js',
-          'division-simple/js/*.js',
           'themes/captureiowa/js/*.js'
         ],
         dest: 'themes/captureiowa/js/build/main.js'
@@ -25,15 +26,18 @@ module.exports = function(grunt) {
       }
     },
 
-    compass: {
-      dist: {                   // Target
+    sass: {
+      dist: { 
+        files: {
+          '<%=globalConfig.themeDir %>/css/master.css' : '<%=globalConfig.themeDir %>/scss/master.scss'
+        },                  // Target
         options: {              // Target options
-          sassDir: 'themes/captureiowa/scss',
-          cssDir: 'themes/captureiowa/css',
-          environment: 'production'
+          style: 'compressed',
+          loadPath: ['bower_components/bootstrap-sass/assets/stylesheets/']
         }
       }
     },
+
 
     imagemin: {
       dynamic: {
@@ -56,7 +60,7 @@ module.exports = function(grunt) {
       },
       css: {
         files: ['themes/captureiowa/scss/*.scss', 'themes/captureiowa/scss/**/*.scss'],
-        tasks: ['compass'],
+        tasks: ['sass'],
         options: {
           spawn: false,
         }
@@ -68,12 +72,12 @@ module.exports = function(grunt) {
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-simple-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['concat', 'uglify', 'compass', 'simple-watch']);
+  grunt.registerTask('default', ['concat', 'uglify', 'sass', 'simple-watch']);
 
 };
